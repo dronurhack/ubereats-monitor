704774cdd321f89786540c63ff2ef0047fa77f81
dronurhack <dronurhack@gmail.com>
2026-07-27 18:26:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -12,21 +12,31 @@ import os
 CITIES = [
     {
         "name": "Lesneven",
-        "slug": "lesneven",
-        "lat": 48.5717,
-        "lon": -4.3261,
+        "url": (
+            "https://www.ubereats.com/fr/feed?diningMode=DELIVERY"
+            "&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMkxlc25ldmVuJTIyJTJDJTIycmVmZXJlbmNlJTIyJTNBJTIyQ2hJSmg1"
+            "OVBKYU9yRmtnUmdHbmxOczJsREFRJTIyJTJDJTIycmVmZXJlbmNlVHlwZSUyMiUzQSUyMmdvb2dsZV9wbGFjZXMl"
+            "MjIlMkMlMjJsYXRpdHVkZSUyMiUzQTQ4LjU3MjA4JTJDJTIybG9uZ2l0dWRlJTIyJTNBLTQuMzIyMjklN0Q%3D"
+        ),
     },
     {
         "name": "Landivisiau",
-        "slug": "landivisiau",
-        "lat": 48.5146,
-        "lon": -4.0672,
+        "url": (
+            "https://www.ubereats.com/fr/feed?diningMode=DELIVERY"
+            "&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMkxhbmRpdmlzaWF1JTIyJTJDJTIycmVmZXJlbmNlJTIyJTNBJTIyQ2hJS"
+            "lI1TnJCRzVRRVVnUlhlVmN3SXBfX1pJJTIyJTJDJTIycmVmZXJlbmNlVHlwZSUyMiUzQSUyMmdvb2dsZV9wbGFjZX"
+            "MlMjIlMkMlMjJsYXRpdHVkZSUyMiUzQTQ4LjUxMDEzMSUyQyUyMmxvbmdpdHVkZSUyMiUzQS00LjA3MzI2NSU3RA%3D%3D"
+        ),
     },
     {
         "name": "Saint-Pol-de-Léon",
-        "slug": "saint-pol-de-leon",
-        "lat": 48.6855,
-        "lon": -3.9847,
+        "url": (
+            "https://www.ubereats.com/fr/feed?diningMode=DELIVERY"
+            "&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMlNhaW50LVBvbC1kZS1MJUMzJUE5b24lMjIlMkMlMjJyZWZlcmVuY2UlMj"
+            "IlM0ElMjJDaElKYl9WRW9pRGlFMGdSd0dIbE5zMmxEQVElMjIlMkMlMjJyZWZlcmVuY2VUeXBlJTIyJTNBJTIyZ29v"
+            "Z2xlX3BsYWNlcyUyMiUyQyUyMmxhdGl0dWRlJTIyJTNBNDguNjg1MTEzJTJDJTIybG9uZ2l0dWRlJTIyJTNBLTMuOT"
+            "g2NTMyOTk5OTk5OTk5NyU3RA%3D%3D"
+        ),
     },
 ]
 
